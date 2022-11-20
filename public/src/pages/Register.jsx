50c4d5ee145888fed7871e784396cc4cd3ae636b
Václav Kesler <vaclav.kesler@gmail.com>
2022-11-20 16:26:37 +0100

    Register form

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import Logo from "../assets/logo.svg"
 
 function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  }
+  const handleChange = (event) => {
+
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <FormContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+          <div className='brand'>
+            <img src={Logo} alt='Logo' />
+            <h1>vMessage</h1>
+          </div>
+          <input type="text" placeholder='Username' name='username' onChange={e=>handleChange(e)}/>
+          <input type="email" placeholder='Email' name='email' onChange={e=>handleChange(e)}/>
+          <input type="password" placeholder='Password' name='password' onChange={e=>handleChange(e)}/>
+          <input type="password" placeholder='Confirm password' name='confirmPassword' onChange={e=>handleChange(e)}/>
+          <button type='submit'>Create User</button>
+          <span>Already have an account? <Link to={"/login"}>Login</Link></span>
+        </form>
+      </FormContainer>
+    </>
   )
 }
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #6482d4;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img{
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #3b3b3b;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid, orange;
+      border-radius: 0.4rem;
+      color: white;
+
+    }
+  }
+`;
+
 
 export default Register
