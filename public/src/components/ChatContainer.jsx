c5bd139816d715bbaa742670d7d222d7620ef2b0
vencoca <vaclav.kesler@gmail.com>
2023-01-03 21:57:55 +0100

    Sockets

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,30 +1,104 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import { useRef } from "react";
 import styled from "styled-components"; 
+import { getAllMessagesRoute, sendMessageRoute } from "../utils/APIRoutes";
+import ChatInput from "./ChatInput";
 import Logout from "./Logout";
+import Messages from "./Messages";
+import {v4 as uuidv4} from "uuid";
 
-export default function ChatContainer({currentChat}) {
-    return (
-        <Container>
-            <div className="chat-header">
-                <div className="user-details">
-                    <div className="avatar">
-                        <img
-                            src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
-                            alt="avatar"
-                        />
+export default function ChatContainer({currentChat,currentUser, socket}) {
+  const [messages, setMessages] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const scrollRef = useRef();
+
+  useEffect( () => {
+    const fetchData = async() => {
+      const data = await JSON.parse(
+        localStorage.getItem("chat-app-user")
+      );
+      const response = await axios.post(getAllMessagesRoute, {
+        from: data._id,
+        to: currentChat._id,
+      });
+      setMessages(response.data);
+    }
+    fetchData();
+  },[currentChat])
+
+
+  const handleSendMsg = async(msg) => {
+    await axios.post(sendMessageRoute,{
+      from:currentUser._id,
+      to:currentChat._id,
+      message: msg,
+    })
+    socket.current.emit("send-msg",{
+      to: currentChat._id,
+      from: currentUser._id,
+      message: msg,
+    });
+
+    const msgs = [... messages];
+    msgs.push({fromSelf:true, message:msg});
+    setMessages(msgs);
+  };
+
+  useEffect( () => {
+    if (socket.current){
+      socket.current.on("msg-recieve",(msg)=>{
+        setArrivalMessage({fromSelf:false, message:msg})
+      })
+    }
+  },[])
+
+  useEffect( () => {
+    arrivalMessage && setMessages((prev)=>[...prev,arrivalMessage]);
+  },[arrivalMessage])
+
+  useEffect( () => {
+    scrollRef.current?.scrollIntoView({behaviour: "smooth"});
+  },[messages])
+
+  return (
+      <Container>
+          <div className="chat-header">
+              <div className="user-details">
+                  <div className="avatar">
+                      <img
+                          src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
+                          alt="avatar"
+                      />
+                  </div>
+                  <div className="username">
+                      <h3>
+                          {currentChat.username}
+                      </h3>
+                  </div>
+              </div>
+              <Logout/>
+          </div>
+          <div className="chat-messages">
+            {
+              messages.map((message) => {
+                return (
+                  <div ref={scrollRef} key={uuidv4()}>
+                    <div className={`message ${message.fromSelf ? "sended":"recieved"}`}>
+                      <div className="content">
+                        <p>
+                          {message.message}
+                        </p>
+                      </div>
                     </div>
-                    <div className="username">
-                        <h3>
-                            {currentChat.username}
-                        </h3>
-                    </div>
-                </div>
-                <Logout/>
-            </div>
-            <div className="chat-messages"></div>
-            <div className="chat-input"></div>
-        </Container>
-    ) 
+                  </div>
+                )
+              })
+            }
+          </div>
+          <ChatInput handleSendMsg={handleSendMsg}/>
+      </Container>
+  ) 
 }
 
 const Container = styled.div`
